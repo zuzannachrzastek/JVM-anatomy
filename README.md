b3306beae3a08e108a650c49ca31dd50ca7ef097
Zuzanna Chrząstek <zuzanna.chrzastek@gmail.com>
2016-10-24 22:05:45 +0200

    Printing method's name and arguments

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # JVM-anatomy
+
+###Requirements
+- JDK 1.6
+- apache ant
+- BCEL library
+
+###Running
+
+To run each of the programs go to their root directory and run:
+
+> ant compile
+> cd classes
+> javac Test.java
+> java -cp ".;../lib/*" Transform Test
+
+To see if it works run:
+
+> java Test
--- zadanie1-1/src/Transform.java
+++ zadanie1-1/src/Transform.java
@@ -7,9 +7,9 @@ import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
 
-import java.io.IOException;
-
 public class Transform {
+
+    private static Transform transform = new Transform();
 
     public static void main(String[] args) {
 
@@ -25,9 +25,72 @@ public class Transform {
 
         ClassGen modClass = new ClassGen(clazz);
         ConstantPoolGen cp = modClass.getConstantPool();
-
         InstructionList il = new InstructionList();
+        InstructionFactory factory = new InstructionFactory(modClass, cp);
 
+        /*
+         * PARAMETERS NEEDED:
+         * method name, arguments' types, return value type
+         * e.g. Method to be called: someMethod(Ljava/lang/String;)Ljava/lang/String;
+         */
+
+        MethodGen methodGen;
+
+        methodGen = transform.createStatementMethod(il, cp, className, factory);
+
+        modClass.addMethod(methodGen.getMethod());
+        modClass.update();
+
+//        InvokeInstruction callStatement = factory.createInvoke(className,
+//                "statement",
+//                Type.VOID,
+//                new Type[]{new ArrayType(Type.STRING, 1)},
+//                Constants.INVOKEVIRTUAL);
+
+//        try {
+//            JavaClass newClass = modClass.getJavaClass();
+//            String className2 = className.replace(".", "/");
+//            newClass.dump(className2 + ".class");
+//            System.out.println("Class " + className + " modified\n");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        for(Method m : clazz.getMethods()){
+            if(m.getReturnType().toString() != "void"){
+                System.out.println("Method's name: " + m.getName().toString());
+                System.out.println("Type of method: " + m.getReturnType().toString());
+                System.out.println("Arguments' types:");
+
+                Type[] types = m.getArgumentTypes();
+                if(types.length > 0) {
+                    for(int i = 0; i < types.length; i++){
+                        System.out.println(types[i].toString());
+                    }
+                } else {
+                    System.out.println("no arguments");
+                }
+                Instruction fieldInstruction = null;
+                MethodGen mg = new MethodGen(m, clazz.getClassName(), cp);
+                InstructionList instructionList  = mg.getInstructionList();
+                InstructionHandle[] ihs = instructionList.getInstructionHandles();
+
+                for(int f = 0; f < ihs.length; f++){
+                    if(ihs[f].getInstruction() instanceof INVOKESTATIC
+                            || ihs[f].getInstruction() instanceof  LDC
+                            || ihs[f].getInstruction() instanceof INVOKEVIRTUAL
+                            || ihs[f].getInstruction() instanceof INVOKEINTERFACE){
+                        fieldInstruction = ihs[f].getInstruction();
+//                        instructionList.insert(fieldInstruction, callStatement);
+                        System.out.println("Found the invoke " + fieldInstruction.toString() + "\n");
+                    }
+                }
+            }
+        }
+
+    }
+
+    private MethodGen createStatementMethod(InstructionList il, ConstantPoolGen cp, String className, InstructionFactory factory){
         il.append(new GETSTATIC(cp.addFieldref("java.lang.System", "out", "Ljava/io/PrintStrem;")));
         il.append(new PUSH(cp, "Is it working??"));
         il.append(new INVOKEVIRTUAL(cp.addMethodref("java.io.PrintStrem", "println", "(Ljava/lang/String;)V")));
@@ -38,7 +101,7 @@ public class Transform {
                 Type.VOID,
                 new Type[]{new ArrayType(Type.STRING, 1)},
                 null,
-                "communicate",
+                "statement",
                 className,
                 il,
                 cp);
@@ -46,36 +109,6 @@ public class Transform {
         methodGen.setMaxLocals();
         methodGen.setMaxStack();
 
-        modClass.addMethod(methodGen.getMethod());
-        modClass.update();
-
-        try {
-            JavaClass newClass = modClass.getJavaClass();
-            String className2 = className.replace(".", "/");
-            newClass.dump(className2 + ".class");
-            System.out.println("Class " + className + " modified");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for(Method m : clazz.getMethods()){
-            if(m.getReturnType().toString() != "void"){
-                System.out.println("Type of method: " + m.getReturnType().toString());
-                MethodGen mg = new MethodGen(m, clazz.getClassName(), cp);
-                InstructionList instructionList  = mg.getInstructionList();
-                InstructionHandle[] ihs = instructionList.getInstructionHandles();
-                Instruction fieldInstruction = null;
-
-                for(int f = 0; f < ihs.length; f++){
-                    if(ihs[f].getInstruction() instanceof INVOKESTATIC
-                            || ihs[f].getInstruction() instanceof  LDC
-                            || ihs[f].getInstruction() instanceof INVOKEVIRTUAL){
-                        fieldInstruction = ihs[f].getInstruction();
-                        System.out.println("Found the invoke " + fieldInstruction.toString());
-                    }
-                }
-            }
-        }
-
+        return methodGen;
     }
 }
