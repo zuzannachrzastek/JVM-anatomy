dd89d41f30ed55f60d48944acff3678a3acd5c38
Zuzanna Chrząstek <zuzanna.chrzastek@gmail.com>
2016-10-25 22:34:26 +0200

    Printing info before calling methods

--- zadanie1-1/classes/Test.java
+++ zadanie1-1/classes/Test.java
@@ -1,22 +1,24 @@
 public class Test {
 
+    private static Test test = new Test();
+
     public static void main(String[] args){
         System.out.println("Hello, I'm MAIN");
         natural(5, 2);
-        System.out.println(hello());
+        System.out.println(hello("Hello from the other side"));
     }
 
     static int natural(int a, int b){
-        hi();
+        test.hi();
         return a + b;
     }
 
-    static String hi(){
+    String hi(){
         String s = "HI naturally!";
         return s;
     }
 
-    static String hello(){
-        return "Hello from the other side";
+    static String hello(String s){
+        return s;
     }
 }
--- zadanie1-1/src/Transform.java
+++ zadanie1-1/src/Transform.java
@@ -2,22 +2,21 @@
  * Created by Zuzanna on 14.10.2016.
  */
 
-import org.apache.bcel.Constants;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
 
-import javax.swing.text.html.HTMLDocument;
-import java.util.Iterator;
+import java.io.IOException;
 
 public class Transform {
 
-    private static Transform transform = new Transform();
+//    private static Transform transform = new Transform();
 
     public static void main(String[] args) throws TargetLostException {
 
         String className = (args.length >= 1) ? args[0] : "";
         JavaClass clazz = null;
+        String mesg;
 
         try {
             clazz = Repository.lookupClass(className);
@@ -31,92 +30,63 @@ public class Transform {
         ConstantPoolGen cpg = new ConstantPoolGen(cp);
         Method[] methods = clazz.getMethods();
 
-        InstructionList il = new InstructionList();
-        il.append(new GETSTATIC(cpg.addFieldref("java.lang.System", "out", "Ljava/io/PrintStrem;")));
-        il.append(new PUSH(cpg, "Method to be called: "));
-        il.append(new INVOKEVIRTUAL(cpg.addMethodref("java.io.PrintStrem", "println", "(Ljava/lang/String;)V")));
-        il.append(new RETURN());
-
-//        InstructionHandle[] ihs = il.getInstructionHandles();
-//        il.delete(ihs[0], ihs[ihs.length]);
-
-        InstructionFactory factory = new InstructionFactory(modClass, cpg);
-
-        /*
-         * PARAMETERS NEEDED:
-         * method name, arguments' types, return value type
-         * e.g. Method to be called: someMethod(Ljava/lang/String;)Ljava/lang/String;
-         */
+        InstructionFactory factory = new InstructionFactory(modClass);
 
         for(Method m : methods){
             MethodGen mg = new MethodGen(m, m.getName(), cpg);
             System.out.println("\nMethod's name: " + m.getName().toString());
             System.out.println("Type of method: " + m.getReturnType().toString());
-            System.out.println("Arguments' types:");
 
             InstructionList mgInsList = mg.getInstructionList();
             InstructionHandle[] mgInsHandles = mgInsList.getInstructionHandles();
 
-            for(Iterator i = mgInsList.iterator(); i.hasNext(); ){
-                InstructionHandle handle = (InstructionHandle) i.next();
+//            for(Iterator i = mgInsList.iterator(); i.hasNext(); ){
+            for(InstructionHandle handle : mgInsHandles){
+//                InstructionHandle handle = (InstructionHandle) i.next();
                 if(handle.getInstruction() instanceof InvokeInstruction){
                     InvokeInstruction invokeInstruction = (InvokeInstruction) handle.getInstruction();
-                    String calledClass = invokeInstruction.getClassName(modClass.getConstantPool());
-                    String calledMethodName = invokeInstruction.getName(modClass.getConstantPool());
+                    String calledMethodName = invokeInstruction.getMethodName(modClass.getConstantPool());
                     String calledMethodSignature = invokeInstruction.getSignature(modClass.getConstantPool());
-                    boolean isStaticMethod = invokeInstruction.getOpcode() == Constants.INVOKESTATIC;
 
-                    System.out.println("Called call: " + calledClass);
-                    System.out.println("Called method name: " + calledMethodName);
-                    System.out.println("Called method signature: " + calledMethodSignature);
+                    mesg = "Method to be called: " + calledMethodName + calledMethodSignature;
+
+                    mgInsList.insert(handle, factory.createPrintln(mesg));
+
+//                    for(Method m1 : methods){
+//                        if(calledMethodName == m1.getName().toString() && m1.getReturnType() != Type.VOID){
+//                            System.out.println("Called method name: " + m1.getName().toString());
+//                            System.out.println("Called method signature: " + m1.getSignature().toString());
+//
+//
+//                            InstructionList il = new InstructionList();
+//                            il.append(new GETSTATIC(cpg.addFieldref("java.lang.System", "out", "Ljava/io/PrintStrem;")));
+//                            il.append(new PUSH(cpg, mesg));
+//                            il.append(new INVOKEVIRTUAL(cpg.addMethodref("java.io.PrintStrem", "println", "(Ljava/lang/String;)V")));
+//
+//                            System.out.println("il: " + il.toString());
+//                            System.out.println("handle: " + handle);
+//
+//                            mgInsList.insert(il);
+//                            mgInsList.update();
+//                            mg.update();
+//                            break;
+//                        }
+//                    }
                 }
-
-
-//                if(ih.getInstruction() instanceof INVOKESTATIC){
-//                    INVOKESTATIC newInst = ((INVOKESTATIC)ih.getInstruction());
-//                    String name = cp.getConstantString(((ConstantMethodref)cp.getConstant(newInst.getIndex())).getClassIndex(), Constants.CONSTANT_Class);
-//                    System.out.println("Static call: " + name);
-//                }
             }
+
+            mg.setMaxStack();
+            modClass.replaceMethod(m, mg.getMethod());
+        }
+//        modClass.update();
+//        clazz.setConstantPool(cpg.getFinalConstantPool());
+        
+        String classFilePath = Repository.lookupClassFile(modClass.getClassName()).getPath();
+        try {
+            modClass.getJavaClass().dump(classFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-//        for(Method m : methods){
-//            MethodGen mg = new MethodGen(m, m.getName(), cpg);
-//            if(m.getReturnType().toString() != "void"){
-//                System.out.println("\nMethod's name: " + m.getName().toString());
-//                System.out.println("Type of method: " + m.getReturnType().toString());
-//                System.out.println("Arguments' types:");
-//
-//                Type[] types = m.getArgumentTypes();
-//                if(types.length > 0) {
-//                    for(int i = 0; i < types.length; i++){
-//                        System.out.println(types[i].toString());
-//                    }
-//                } else {
-//                    System.out.println("no arguments");
-//                }
-//                Instruction fieldInstruction = null;
-//
-//                InstructionList mgInsList = mg.getInstructionList();
-//                InstructionHandle[] mgInsHandles = mgInsList.getInstructionHandles();
-//                InstructionHandle ih = mgInsList.getStart();
-//
-//                for(int f = 0; f < mgInsHandles.length; f++){
-//
-//                    System.out.println("ins. " + f + ": " + mgInsHandles[f].getInstruction().toString());
-//
-//                    if(mgInsHandles[f].getInstruction() instanceof INVOKESTATIC){
-//                        INVOKESTATIC newInst = ((INVOKESTATIC)mgInsHandles[f].getInstruction());
-//                        name = cp.getConstantString(((ConstantMethodref)cp.getConstant(newInst.getIndex())).getClassIndex(), Constants.CONSTANT_Class);
-//                        System.out.println("Static call: " + name);
-////                        fieldInstruction = mgInsHandles[f].getInstruction();
-////                        instructionList.insert(fieldInstruction, callStatement);
-////                        System.out.println("Found the invoke " + fieldInstruction.toString());
-//                    }
-//                }
-//            }
-//        }
-
-//        modClass.update();
     }
 }
