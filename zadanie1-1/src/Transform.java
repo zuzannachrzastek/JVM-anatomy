48c816fd160bba07156d77148c35fbdda23e08ed
Zuzanna Chrząstek <zuzanna.chrzastek@gmail.com>
2016-10-26 18:36:02 +0200

    Return value printing

--- zadanie1-1/src/Transform.java
+++ zadanie1-1/src/Transform.java
@@ -2,15 +2,15 @@
  * Created by Zuzanna on 14.10.2016.
  */
 
+import org.apache.bcel.Constants;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Transform {
-
-//    private static Transform transform = new Transform();
 
     public static void main(String[] args) throws TargetLostException {
 
@@ -26,34 +26,79 @@ public class Transform {
         }
 
         ClassGen modClass = new ClassGen(clazz);
-        ConstantPool cp = clazz.getConstantPool();
-        ConstantPoolGen cpg = new ConstantPoolGen(cp);
+        ConstantPoolGen cpg = modClass.getConstantPool();
         Method[] methods = clazz.getMethods();
 
-        InstructionFactory factory = new InstructionFactory(modClass);
-
         for(Method m : methods){
+
             MethodGen mg = new MethodGen(m, m.getName(), cpg);
             System.out.println("\nMethod's name: " + m.getName().toString());
-            System.out.println("Type of method: " + m.getReturnType().toString());
+            System.out.println("Return type: " + m.getReturnType().toString());
 
+            InstructionFactory factory = new InstructionFactory(modClass);
             InstructionList mgInsList = mg.getInstructionList();
             InstructionHandle[] mgInsHandles = mgInsList.getInstructionHandles();
 
-//            for(Iterator i = mgInsList.iterator(); i.hasNext(); ){
             for(InstructionHandle handle : mgInsHandles){
-//                InstructionHandle handle = (InstructionHandle) i.next();
+
                 if(handle.getInstruction() instanceof InvokeInstruction){
                     InvokeInstruction invokeInstruction = (InvokeInstruction) handle.getInstruction();
-                    String calledMethodName = invokeInstruction.getMethodName(modClass.getConstantPool());
-                    String calledMethodSignature = invokeInstruction.getSignature(modClass.getConstantPool());
+                    String calledMethodName = invokeInstruction.getMethodName(cpg);
+                    String calledMethodSignature = invokeInstruction.getSignature(cpg);
 
                     mesg = "Method to be called: " + calledMethodName + calledMethodSignature;
 
                     mgInsList.insert(handle, factory.createPrintln(mesg));
+
+                    if(!(invokeInstruction instanceof INVOKESPECIAL)){
+                        Type returnType = invokeInstruction.getReturnType(cpg);
+                        InstructionList returnInsList = new InstructionList();
+
+                        if(returnType == Type.VOID) {
+                            returnInsList.append(factory.createPrintln("Got result: "));
+                        } else {
+                            returnInsList.append(factory.createFieldAccess(
+                                    System.class.getCanonicalName(),
+                                    "out",
+                                    new ObjectType(PrintStream.class.getCanonicalName()),
+                                    Constants.GETSTATIC
+                            ));
+                            returnInsList.append(factory.createConstant("Got result: "));
+                            returnInsList.append(factory.createInvoke(
+                                    PrintStream.class.getCanonicalName(),
+                                    "print",
+                                    Type.VOID,
+                                    new Type[]{new ObjectType("java.lang.String")},
+                                    Constants.INVOKEVIRTUAL
+                            ));
+                            returnInsList.append(InstructionFactory.createDup(returnType.getSize()));
+                            returnInsList.append(factory.createFieldAccess(
+                                    System.class.getCanonicalName(),
+                                    "out",
+                                    new ObjectType(PrintStream.class.getCanonicalName()),
+                                    Constants.GETSTATIC
+                            ));
+
+                            if(returnType.equals(Type.DOUBLE) || returnType.equals(Type.LONG)){
+                                returnInsList.append(InstructionFactory.DUP_X2);
+                                returnInsList.append(InstructionFactory.POP);
+                            } else {
+                                returnInsList.append(InstructionFactory.SWAP);
+                            }
+
+                            InvokeInstruction callInvoke = factory.createInvoke(
+                                    PrintStream.class.getCanonicalName(),
+                                    "println",
+                                    Type.VOID,
+                                    new Type[]{new ObjectType("java.lang.Object")},
+                                    Constants.INVOKEVIRTUAL
+                            );
+                            returnInsList.append(callInvoke);
+                        }
+                        mgInsList.append(handle, returnInsList);
+                    }
                 }
             }
-
             mg.setMaxStack();
             modClass.replaceMethod(m, mg.getMethod());
         }
@@ -64,6 +109,5 @@ public class Transform {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
